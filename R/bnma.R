4f6e567b094d552730b5b49165f0478d1b0d3567
Michael Seo <swj8874@gmail.com>
2019-03-14 17:28:21 +0100

    Initial commit

---
+++ R/bnma.R
@@ -0,0 +1,98 @@
+#' bnma: A package for network meta analysis using Bayesian methods
+#'
+#' A package for running Bayesian network meta analysis
+#'
+#' Network meta-analysis or mixed treatment comparison (MTC) is a method that allows simultaneous comparison of more than two treatments.
+#' We use a Bayesian approach to combine both direct and indirect evidence as in Dias et al. 2013a.
+#' This package is a user friendly application that can run network meta analysis models without having to code a JAGS model.
+#' The program takes the input data and transforms it to a suitable format of analysis, generates a JAGS model and reasonable
+#' initial values and runs the model through the rjags package.
+#' The focus of this package was inclusion of multinomial response and various options for adding covariates and/or baseline risks effects.
+#' Also, while sampling, the package uses Gelman-Rubin convergence criteria to decide whether to continue sampling or not.
+#' Furthermore, package includes different models such as contrast based models and unrelated mean effects (UME) model and nodesplitting model to test for inconsistency.
+#'
+#' @docType package
+#' @name bnma-package
+#' @references A.J. Franchini, S. Dias, A.E. Ades, J.P. Jansen, N.J. Welton (2012), \emph{Accounting for correlation in network meta-analysis with multi-arm trials}, Research Synthesis Methods 3(2):142-160. [\url{https://doi.org/10.1002/jrsm.1049}]
+#' @references S. Dias, A.J. Sutton, A.E. Ades, and N.J. Welton (2013a), \emph{A Generalized Linear Modeling Framework for Pairwise and Network Meta-analysis of Randomized Controlled Trials}, Medical Decision Making 33(5):607-617. [\url{https://doi.org/10.1177/0272989X12458724}]
+#' @references S. Dias, A.J. Sutton, A.E. Ades, and N.J. Welton (2013b), \emph{Heterogeneity-Subgroups, Meta-Regression, Bias, and Bias-Adjustment}, Medical Decision Making 33(5):618-640. [\url{https://doi.org/10.1177/0272989X13485157}]
+#' @references S. Dias, N.J. Welton, D.M. Caldwellb, A.E. Ades (2010), \emph{Checking consistency in mixed treatment}, Statistics in Medicine 29(7-8, Sp. Iss. SI): 932-944. [\url{https://doi.org/10.1002/sim.3767}]
+#' @references S. Dias, N.J. Welton, A.J. Sutton, D.M. Caldwell, G. Lu, and A.E. Ades (2013), \emph{Evidence synthesis for decision making 4: inconsistency in networks of evidence based on randomized controlled trials}, Medical Decision Making 33(5):641-656. [\url{https://doi.org/10.1177/0272989X12455847}]
+#' @references C.H. Schmid, T.A. Trikalinos, I. Olkin (2014), \emph{Bayesian network meta-analysis for unordered categorical outcomes with incomplete data}, Research Synthesis Methods 5(2):162-185. [\url{https://doi.org/10.1002/jrsm.1103}]
+#' @references A. Gelman, D.B. Rubin (1992), \emph{Inference from iterative simulation using multiple sequences}, Statistical Science 7(4):457-472. [\url{http://dx.doi.org/10.1214/ss/1177011136}]
+#' @references D.J. Spiegelhalter, N.G. Best, and B.P. Carlin (1998), \emph{Bayesian deviance, the effective nunmber of parameters, and the comparison of arbitrarily complex models}, Technical report, MRC Biostatistics Unit, Cambridge, UK.
+#' @references F.A. Achana, N.J. Cooper, S. Dias, G. Lu, S.J.C. Rice, D. Kendrick, A.J. Sutton (2012), \emph{Extending methods for investigating the relationship between treatment effect and baseline risk from pairwise meta-analysis to network meta-analysis}, Statistics in Medicine 32(5):752-771. [\url{https://doi.org/10.1002/sim.5539}]
+#' @references F.A. Achana, N.J. Cooper, S. Bujkiewicz, S.J. Hubbard, D. Kendrick, D.R. Jones, A.J. Sutton (2014), \emph{Network meta-analysis of multiple outcomes measures accounting for borrowing of information across outcomes}, BMC Medical Research Methodology 14:92. [\url{https://doi.org/10.1186/1471-2288-14-92}]
+#' @references G. Salanti, A.E. Ades, J.P.A. Ioannidisa (2011), \emph{Graphical methods and numerical summaries for presenting results from multiple-treatment meta-analysis: an overview and tutorial}, Journal of Clinical Epidemiology 64(2):163-171. [\url{https://doi.org/10.1016/j.jclinepi.2010.03.016}]
+#' @references G. van Valkenhoef, G. Lu, B. de Brock, H. Hillege, A.E. Ades, and N.J. Welton (2012), \emph{Automating network meta-analysis}, Research Synthesis Methods 3(4):285-299. [\url{https://doi.org/10.1002/jrsm.1054}]
+#' @references N.J. Cooper, A.J. Sutton, D. Morris, A.E. Ades, N.J. Welton (2009), \emph{Addressing between-study heterogeneity and inconsistency in mixed treatment comparisons: Application to stroke prevention treatments in individuals with non-rheumatic atrial fibrillation}, Statistics in Medicine 28:1861-1881. [\url{https://doi.org/10.1002/sim.3594}]
+#' @references W. Viechtbauer (2010), \emph{Conducting meta-analyses in R with the metafor package}, Journal of Statistical Software, 36(3):1-48. [\url{https://doi.org/10.18637/jss.v036.i03}]
+#' @seealso \code{\link{network.data}}, \code{\link{network.run}}
+NULL
+
+#' Beta blockers to prevent mortality after myocardial infarction
+#'
+#' A dataset of 22 trials investigating beta blockers versus control to prevent mortality after myocardial
+#' infarction. Control is coded as 1 and beta blocker treatment is coded as 2.
+#' @format A list of Outcomes, Treat, Study, and N.
+"blocker"
+
+#' Trials of certolizumab pegol (CZP) for the treatment of rheumatoid arthritis in patients
+#'
+#' A dataset of 12 trials for investigating CZP for the treatment for those who had failed on disease-modifying antirheumatic drugs, including methotrexate (MTX).
+#' Data provides the number of patients who have improved and there are 6 different treatments with placebo. Mean disease duration (years) is provided as a covariate.
+#'
+#' @references S. Dias, A.J. Sutton, A.E. Ades, and N.J. Welton (2013b), \emph{Heterogeneity-Subgroups, Meta-Regression, Bias, and Bias-Adjustment}, Medical Decision Making 33(5):618-640. [\url{https://doi.org/10.1177/0272989X13485157}]
+#' @format A list of Outcomes, Treat, Study, N, covariate, and Treat.order
+"certolizumab"
+
+#' Trials of statins for cholesterol lowering vs. placebo or usual care
+#'
+#' A dataset of 19 trials of statins for cholesterol lowering vs. placebo.
+#' Each trial has a subgroup indicator for primary prevention (patients included had no previous heart disease) or
+#' secondary prevention (patients had previous heart disease). Dummy variable is coded such that covariate is equal to 1
+#' if a study is a secondary prevention study and 0 if a study is a primary prevention study.
+#'
+#' @references S. Dias, A.J. Sutton, A.E. Ades, and N.J. Welton (2013b), \emph{Heterogeneity-Subgroups, Meta-Regression, Bias, and Bias-Adjustment}, Medical Decision Making 33(5):618-640. [\url{https://doi.org/10.1177/0272989X13485157}]
+#' @format A list of Outcomes, Treat, Study, N, covariate, and Treat.order
+"statins"
+
+#' Dopamine agonists as adjunct therapy in Parkinson's disease
+#'
+#' A dataset of 7 studies investigating the mean lost work-time reduction in patients given
+#' 4 dopamine agonists and placebo as adjunct therapy for Parkinson's disease.
+#' There is placebo, coded as 1, and four active drugs coded 2 to 5.
+#'
+#' @references S. Dias, A.J. Sutton, A.E. Ades, and N.J. Welton (2013a), \emph{A Generalized Linear Modeling Framework for Pairwise and Network Meta-analysis of Randomized Controlled Trials}, Medical Decision Making 33(5):607-617. [\url{https://doi.org/10.1177/0272989X12458724}]
+#' @format A list of Outcomes, Treat, Study, N, covariate, and Treat.order
+"parkinsons"
+
+#' Trials of low dose and high dose statins for cardiovascular disease vs. placebo
+#'
+#' A dataset of 17 studies investigating dosage of statin for cardiovascular disease.
+#' There are two treatments and a placebo. High dose statin is coded as 3, low dose statin as 2, and placebo is coded as 1 and treated as a baseline treatment.
+#' Outcomes are reported as three mutually exclusive unordered outcomes.
+#' First column of the outcome is the patients who are still alive (ALIVE). Second column is fatal non-cardiovascular disease (FnCVD).
+#' And, the last column is fatal cardiovascular disease (FCVD).
+#'
+#' @references C.H. Schmid, T.A. Trikalinos, I. Olkin (2014), \emph{Bayesian network meta-analysis for unordered categorical outcomes with incomplete data}, Research Synthesis Methods 5(2):162-185. [\url{https://doi.org/10.1002/jrsm.1103}]
+#' @format A list of Outcomes, Treat, Study, and N
+"cardiovascular"
+
+#' @importFrom graphics axis legend lines mtext plot points title
+NULL
+
+#' @importFrom stats coef end lm quantile rchisq rnorm sd start window
+NULL
+
+#' @importFrom utils combn
+NULL
+
+#' @import coda
+NULL
+
+#' @import ggplot2
+NULL
+
+#' @import grid
+NULL
---
+++ R/preprocess.data.R
@@ -0,0 +1,165 @@
+#' Preprocess data given user input
+#'
+#' This is first bit of our code that runs when the user enters the data using \code{\link{network.data}}.
+#' The function preprocesses the given data to fit the data format necessary to run our NMA model in JAGS.
+#' It is essential to know how the data is preprocessed to obtain correct analysis of interest.
+#' Thus, we have exported the function so that user can have better understanding on what is going on underneath.
+#'
+#' @export
+
+preprocess.data <- function(Outcomes = NULL, Study = NULL, Treat = NULL, N = NULL, SE = NULL, response = NULL, Treat.order = NULL, type = "random",
+                            baseline = "none", baseline.risk = "independent", covariate = NULL, covariate.type = NULL, covariate.model = NULL){
+
+  network <- list(Outcomes = Outcomes, Study = Study, Treat = Treat, N = N, SE = SE, response = response, Treat.order = Treat.order, type = type,
+                  baseline = baseline, baseline.risk = baseline.risk, covariate = covariate, covariate.type = covariate.type, covariate.model = covariate.model)
+
+  check.for.errors(network) # check for errors in the data specified
+
+  # changes treatment and study names and order it based on treat.order or alphabetical order if left unspecified
+  transform <- transform.data(network)
+  data <- transform$data
+
+  # If baseline.risk = "exchangeable", add a fictitious arm with overall reference treatment
+  if(baseline.risk == "exchangeable"){
+    data <- add.fictitious.row(network, data)
+  }
+
+  list(data = data, Treat.order = transform$Treat.order, Study.order = transform$Study.order,
+       response = response, Treat.order = Treat.order, type = type, baseline = baseline, baseline.risk = baseline.risk,
+       covariate = covariate, covariate.type = covariate.type, covariate.model = covariate.model)
+
+}
+
+add.fictitious.row <- function(network, data){
+
+  with(network,{
+    no_reference <- vector(mode = "integer")
+    for(i in seq(length(unique(Study)))){
+      if(!1 %in% data[data[,"Study"] == i, "Treat"]){
+        no_reference <- c(no_reference, i)
+      }
+    }
+
+    store <- vector(mode = "integer")
+    ncol <- dim(as.matrix(Outcomes))[2]
+    if(length(no_reference) != 0){
+      for(i in 1:length(no_reference)){
+        store <- rbind(store, c(rep(NA, ncol), 1, no_reference[i], 1))
+      }
+    }
+
+    if(length(store) != 0){
+      colnames(store) <- colnames(data)
+      data <- rbind(data, store)
+    }
+
+    #reorder the data
+    ordering <- order(data[,"Study"], data[,"Treat"])
+    data <- data[ordering,]
+    return(data)
+  })
+}
+
+
+transform.data <- function(network){
+
+  with(network, {
+
+    Orig_Study <- Study
+    Orig_Treat <- Treat
+
+    #relabel Study names into to a numeric sequence (1:nstudy)
+    na <- rle(Study)$lengths
+    Study.order <- unique(Study)
+    names(Study.order) <- 1:length(Study.order)
+    Study <- rep(1:length(unique(Study)), times = na)
+
+    #relabel the treatment according to treatment order specified
+    if(is.null(Treat.order)){
+      Treat.order <- sort(unique(Treat))
+    }
+    Treat <- relabel.vec(Treat, Treat.order)
+    names(Treat.order) <- 1:length(Treat.order)
+
+    data <- if(response == "normal"){
+      cbind(Outcomes, SE, Study, Treat)
+    } else {
+      cbind(Outcomes, N, Study, Treat)
+    }
+
+    ordering <- order(Study, Treat)
+    data <- data[ordering,]
+
+    return(list(data = data, Treat.order = Treat.order, Study.order = Study.order))
+  })
+}
+
+
+relabel.vec <- function(x, order)
+{
+  old.x <- x
+  x <- rep(NA, length(old.x))
+  for (i in seq(length(order))) x[old.x == order[i]] <- i #relabel studies in numerical order starting with one
+  return(x)
+}
+
+
+check.for.errors <- function(network){
+
+  with(network, {
+
+    if(is.null(Study) || is.null(Treat) || is.null(Outcomes)){
+      stop("Study, Treat, and Outcomes have to be all specified")
+    }
+
+    if(response == "multinomial" || response == "binomial"){
+      if(is.null(N)){
+        stop("If the response is multinomial or binomial, N has to be specified")
+      }
+    } else if (response == "normal"){
+      if(is.null(SE)){
+        stop("If the response is normal, SE has to be specified")
+      }
+    }
+
+    if(is.null(baseline) || !baseline %in% c("none", "independent", "exchangeable", "common")){
+      stop("baseline has to be none, independent, exchangeable, or common")
+    }
+
+    if(is.null(baseline.risk) || !baseline.risk %in% c("independent", "exchangeable")){
+      stop("baseline risk has to be independent or exchangeable")
+    }
+
+    if(!type %in% c("fixed", "random")){
+      stop("type has to be either fixed or random")
+    }
+
+    if(!is.null(covariate)){
+      if(!is.matrix(covariate) && !is.vector(covariate)){
+        stop("covariate has to be vector if there is only one and it is a matrix if there is more than one")
+      }
+      if(is.null(covariate.model)){
+        covariate.model <- "common"
+      }
+      covariate <- as.matrix(covariate)
+      if(is.null(covariate.type)){
+        covariate.type = rep("continuous", dim(covariate)[2])
+      }
+      if(!all(apply(covariate, 2, is.numeric))){
+        stop("covariate has to be numeric type")
+      }
+    }
+
+    if(!is.null(Treat.order)){
+      if(length(unique(Treat.order)) != length(unique(Treat))){
+        stop("Need to specify treatment order for all treatments")
+      }
+      if(!all(Treat.order %in% Treat)){
+        stop("Treat.order names have to be specified correctly.")
+      }
+    }
+
+    na <- rle(Study)$lengths
+    if(any(na == 1)) stop("study cannot have only 1 arm or arms have to be next to each other in each study")
+  })
+}

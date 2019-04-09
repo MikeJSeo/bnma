f503263273e2a748f110e47d934f9a0de76ec7e9
Michael Seo <swj8874@gmail.com>
2019-04-09 23:28:52 +0200

    revision

--- R/network.run.R
+++ R/network.run.R
@@ -59,7 +59,7 @@ network.run <- function(network, inits = NULL, n.chains = 3, max.run = 100000, s
     
     prior.data <- list(mean.d = mean.d, prec.d = prec.d, mean.Eta = mean.Eta, prec.Eta = prec.Eta, 
                       hy.prior.1 = hy.prior.1, hy.prior.2 = hy.prior.2, mean.bl = mean.bl, prec.bl = prec.bl,
-                      hy.prior.Eta.1 = hy.prior.Eta.2, hy.prior.Eta.2 = hy.prior.Eta.2
+                      hy.prior.Eta.1 = hy.prior.Eta.1, hy.prior.Eta.2 = hy.prior.Eta.2
                       ) # need to update
     data <- append(data, prior.data)
     data <- data[!is.na(data)]

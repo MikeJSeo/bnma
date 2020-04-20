802e05bc812ee0867053220b2b5ab085a487e91c
Michael Seo <swj8874@gmail.com>
2020-04-20 12:08:16 +0200

    revision

--- R/network.data.R
+++ R/network.data.R
@@ -32,6 +32,7 @@
 #' @param prec.cov Prior precision for the covariate effect
 #' @param hy.prior.cov Between treatment heterogeneity in covariate effect (for exchangeable regression coefficient only). Format of the parameter is same as hy.prior. Default is set to be dunif(0, 5) for binary, dunif(0, 100) for normal, and wishart with identity scale matrix and (# of categories - 1) degrees of freedom for multinomial.
 #' @param hy.prior Prior for the heterogeneity parameter. Supports uniform, gamma, and half normal for normal and binomial response and wishart for multinomial response. It should be a list of length 3, where first element should be the distribution (one of dunif, dgamma, dhnorm, dwish) and the next two are the parameters associated with the distribution. For example, list("dunif", 0, 5) give uniform prior with lower bound 0 and upper bound 5 for the heterogeneity parameter. For wishart distribution, the last two parameter would be the scale matrix and the degrees of freedom.
+#' @param A.probability estimate of 'standard' treatment (i.e. placebo) in natural (probability) scale; this is used for binomial outcome when number needed to treat, risk difference, or relative risk is needed; this should be informed from external evidence or can be found by meta-analyzing single proportions
 #' @return Creates list of variables that are used to run the model using \code{\link{network.run}}
 #' \item{data}{Data combining all the input data. User can check this to insure the data is correctly specified. For modelling purposes, character valued studies or treatment variables are changed to numeric values based on alphabetical order.}
 #' \item{nrow}{Total number of arms in the meta-analysis}
@@ -61,12 +62,12 @@
 network.data <- function(Outcomes = NULL, Study = NULL, Treat = NULL, N = NULL, SE = NULL, response = NULL, Treat.order = NULL, type = "random", rank.preference = "higher",
                          baseline = "none", baseline.risk = "independent", covariate = NULL, covariate.type = NULL, covariate.model = NULL,
                          mean.d = NULL, prec.d = NULL, mean.Eta = NULL, prec.Eta = NULL, hy.prior.Eta = NULL, mean.bl = NULL, prec.bl = NULL, hy.prior.bl = NULL,
-                         mean.cov = NULL, prec.cov = NULL, hy.prior.cov = NULL, hy.prior = NULL) {
+                         mean.cov = NULL, prec.cov = NULL, hy.prior.cov = NULL, hy.prior = NULL, A.probability = NULL) {
 
   # rename the variables and order them based on specified treatment order
   network <- preprocess.data(Outcomes = Outcomes, Study = Study, Treat = Treat, N = N, SE = SE, response = response, Treat.order = Treat.order, type = type, rank.preference = rank.preference,
                   baseline = baseline, baseline.risk = baseline.risk, covariate = covariate, covariate.type = covariate.type, covariate.model = covariate.model,
-                  hy.prior.Eta = hy.prior.Eta, hy.prior.bl = hy.prior.bl, hy.prior.cov = hy.prior.cov, hy.prior = hy.prior)
+                  hy.prior.Eta = hy.prior.Eta, hy.prior.bl = hy.prior.bl, hy.prior.cov = hy.prior.cov, hy.prior = hy.prior, A.probability = A.probability)
   
   # find characteristic values associated with the network, i.e. number of studies, number of treatments, etc
   characteristics <- find.characteristics(network)
--- R/network.rjags.R
+++ R/network.rjags.R
@@ -182,6 +182,17 @@ model.binomial <- function(network)
     } else if(type == "fixed"){
       code <- paste0(code, rank.rjags(rank.preference, ntreat))
     }
+    
+    if(!is.null(A.probability)){
+      code <- paste0(code,
+                     "\n\tfor(k in 1:", ntreat, ") { logit(T[k]) <- ", log(A.probability/(1-A.probability)), " + d[k] }",
+                     "\n\tfor(k in 2:", ntreat, ") {",
+                     "\n\t\tNNT[k] <- 1/(T[k] - T[1])",
+                     "\n\t\tRD[k] <- T[k] - T[1]",
+                     "\n\t\tRR[k] <- T[k]/T[1]",
+                     "\n\t}")
+    }
+    
     return(code)
   })
 }

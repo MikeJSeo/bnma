33301ea0f671f7c8e7805d08001b637766c492e7
Michael Seo <swj8874@gmail.com>
2024-02-11 00:44:14 +0100

    rev

--- R/network.summary.R
+++ R/network.summary.R
@@ -931,7 +931,7 @@ variance.tx.effects = function(result)
 #' @examples
 #' network <- with(certolizumab, {
 #'  network.data(Outcomes, Study, Treat, N=N, response="binomial", Treat.order,
-#'  covariate = covariate, hy.prior = list("dhnorm", 0, 9.77))
+#'  covariate = covariate, covariate.type = "continous", hy.prior = list("dhnorm", 0, 9.77))
 #' })
 #' \donttest{
 #' result <- network.run(network)
